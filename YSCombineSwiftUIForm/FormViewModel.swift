a92deb55cd671918842eda20d4abb1bd7718f47d
Jarek Šedý <jareksedy@icloud.com>
2022-02-19 10:56:11 +0600

    Initial Commit

---
+++ YSCombineSwiftUIForm/FormViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FormViewModel.swift
+//  YSCombineSwiftUIForm
+//
+//  Created by Ярослав on 19.02.2022.
+//
+
+import Combine
+
---
+++ YSCombineSwiftUIForm/YSCombineSwiftUIFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  YSCombineSwiftUIFormApp.swift
+//  YSCombineSwiftUIForm
+//
+//  Created by Ярослав on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct YSCombineSwiftUIFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
