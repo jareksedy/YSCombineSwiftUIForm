e1fbc29a1b160f87f2ae2809684f4597238842a4
Jarek Šedý <jareksedy@icloud.com>
2022-02-19 11:44:49 +0600

    Binding ui and viewmodel.

--- YSCombineSwiftUIForm/ContentView.swift
+++ YSCombineSwiftUIForm/ContentView.swift
@@ -9,24 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var formViewModel = FormViewModel()
+    @State var alertPresented = false
     
     var body: some View {
         NavigationView {
             Form {
-                Section("Введите имя пользователя") {
+                Section(header: Text("Введите имя пользователя"), footer: Text(formViewModel.userNameMessage).foregroundColor(.red)) {
                     TextField("Имя пользователя", text: $formViewModel.userName)
                         .autocapitalization(.none)
                 }
                 
-                Section("Введите пароль") {
+                Section(header: Text("Введите пароль"), footer: Text(formViewModel.passwordMessage).foregroundColor(.red)) {
                     SecureField("Пароль", text: $formViewModel.password)
                     SecureField("Пароль повторно", text: $formViewModel.passwordAgain)
                 }
                 
                 Section {
-                    Button(action: {}) { Text("Зарегистрироваться") }.disabled(!self.formViewModel.isValid)
+                    Button(action: { self.alertPresented = true }) { Text("Зарегистрироваться") }.disabled(!self.formViewModel.isValid)
                 }
             }
+            .sheet(isPresented: $alertPresented) { Text("Поздравляем с успешной регистрацией!") }
             .navigationTitle("Регистрация")
             .navigationBarTitleDisplayMode(.large)
         }
